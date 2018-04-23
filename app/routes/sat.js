70196178061442a92f6d5cdbeb68db29fb1914dc
Joel <joeltwaage@gmail.com>
2018-04-23 01:21:46 -0400

    get current date to display in console using moment() call in .js file

--- app/routes/sat.js
+++ app/routes/sat.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import moment from 'moment';
 
 export default Route.extend({
+  moment: Ember.inject.service(),
+  actions: {
+
+    click() {
+      console.log('working');
+    },
+
+    displayDate() {
+      console.log('click');
+      var start = moment().format('MM/DD/YYYY');
+      console.log(start);
+    }
+  }
 });
